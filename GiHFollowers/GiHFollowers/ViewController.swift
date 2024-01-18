acfab21012ef645687721c2b0e17b434ed54f957
Yunus Emre <ye.berdibek@gmail.com>
2024-01-19 00:24:48 +0300

    The project has been created.

---
+++ GiHFollowers/GiHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiHFollowers
+//
+//  Created by Yunus Emre Berdibek on 19.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
