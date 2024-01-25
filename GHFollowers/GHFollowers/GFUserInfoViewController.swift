5e47239146096350c1aac1295562f977606da9a1
Yunus Emre <ye.berdibek@gmail.com>
2024-01-26 01:37:24 +0300

    UserNetworkManager created.

--- GHFollowers/GHFollowers/GFUserInfoViewController.swift
+++ GHFollowers/GHFollowers/GFUserInfoViewController.swift
@@ -14,7 +14,16 @@ final class GFUserInfoViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
-        print(username)
+
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong.", message: error.rawValue, buttonTitle: "OK")
+            }
+        }
     }
 
     @objc
--- GHFollowers/GHFollowers/NetworkManager.swift
+++ GHFollowers/GHFollowers/NetworkManager.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+// TODO: Ilk olarak MVVM Geçirmeden önce bu yapıyı daha generic bir şekilde kurmak lazım. MVC kullanılsa bile her network çağrısı için network manager içerisinde ayrı fonksiyon yazmak avellik.
+
 enum GFNetworkError: String, Error {
     case invalidUsername = "This username created an invalid request please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
@@ -47,6 +49,66 @@ final class NetworkManager {
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
                 completion(.success(followers))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
+
+    func getUserInfo(for username: String, completion: @escaping (Result<User, GFNetworkError>) -> Void) {
+        let endpoint = baseURL + "\(username)"
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidUsername))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            if let _ = error {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completion(.success(user))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        task.resume()
+    }
+
+    // TODO: Bu fonksiyonu kullanarak ilerde daha generic bir network manager yapılabilir.
+    func execute<T: Codable>(with request: URLRequest, expecting type: T.Type, completion: @escaping (Result<T, GFNetworkError>) -> Void) {
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                  response.statusCode == 200
+            else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let result = try decoder.decode(T.self, from: data)
+                completion(.success(result))
             } catch {
                 completion(.failure(.invalidData))
             }
